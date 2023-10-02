27808a9b66f1c783bf44ef089581579c06cc1eb8
Jermvio <nikita.kabuzan@gmail.com>
2023-10-02 21:30:43 +0300

    main cycle while was changed to do while unnecessary in cycle methods was excluded to init block

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class App {
 
-    static Scanner scan = new Scanner(System.in);
-
     public static void main(String[] args) {
         startGame();
     }
@@ -15,46 +13,33 @@ public class App {
     }
 
     private static void startGame(Player firstPlayer, Player secondPlayer) {
-        // init
-
-        boolean boxAvailable = false;
         byte winner = 0;
         char[] box = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
         System.out.println("Enter box number to select. Enjoy!\n");
 
-        boolean boxEmpty = false;
+        printTableInConsole(box);
+        emptyBox(box);
 
-
-        while (true) {
-            printTableInConsole(box);
-
-            boxEmpty = emptyBox(box, boxEmpty);
-
-            if (gameFinished(winner)) {
-                break;
-            }
-
+        do {
             firstPlayer.makeTurn(box);
 
-            if (playerWin('X', box)) {
+            if (checkPlayerWin('X', box)) {
                 winner = 1;
-                continue;
             }
 
-            boxAvailable = isBoxAvailable(box);
-
-            if (checkDraw(boxAvailable)) {
+            if (checkDraw(box, winner)) {
                 winner = 3;
-                continue;
             }
 
             secondPlayer.makeTurn(box);
 
-            if (playerWin('O', box)) {
+            if (checkPlayerWin('O', box)) {
                 winner = 2;
-                continue;
             }
-        }
+
+            printTableInConsole(box);
+        } while (winner == 0);
+        gameFinished(winner);
     }
 
     private static void printTableInConsole(char[] box) {
@@ -65,88 +50,76 @@ public class App {
         System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
     }
 
-    private static boolean emptyBox(char[] box, boolean boxEmpty) {
-        byte i;
-        if (!boxEmpty) {
-            for (i = 0; i < 9; i++)
-                box[i] = ' ';
-            boxEmpty = true;
+    private static void emptyBox(char[] box) {
+        for (int i = 0; i < 9; i++) {
+            box[i] = ' ';
         }
-        return boxEmpty;
     }
 
-    private static boolean gameFinished(byte winner) {
-        boolean gameFinished = false;
+    private static void gameFinished(byte winner) {
         if (winner == 1) {
             System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
-            gameFinished = true;
         } else if (winner == 2) {
             System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
-            gameFinished = true;
         } else if (winner == 3) {
             System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
-            gameFinished = true;
         }
-        return gameFinished;
     }
 
-    private static boolean playerWin(char playerSign, char[] box) {
-        if (    (box[0] == playerSign && box[1] == playerSign && box[2] == playerSign) ||
+    public static boolean checkPlayerWin(char playerSign, char[] box) {
+        return (box[0] == playerSign && box[1] == playerSign && box[2] == playerSign) ||
                 (box[3] == playerSign && box[4] == playerSign && box[5] == playerSign) ||
                 (box[6] == playerSign && box[7] == playerSign && box[8] == playerSign) ||
                 (box[0] == playerSign && box[3] == playerSign && box[6] == playerSign) ||
                 (box[1] == playerSign && box[4] == playerSign && box[7] == playerSign) ||
                 (box[2] == playerSign && box[5] == playerSign && box[8] == playerSign) ||
                 (box[0] == playerSign && box[4] == playerSign && box[8] == playerSign) ||
-                (box[2] == playerSign && box[4] == playerSign && box[6] == playerSign)
-        ) {
-            return true;
-        }
-        return false;
+                (box[2] == playerSign && box[4] == playerSign && box[6] == playerSign);
     }
 
-    private static boolean isBoxAvailable(char[] box) {
-        byte i;
-        boolean boxAvailable;
-        boxAvailable = false;
-        for (i = 0; i < 9; i++) {
-            if (box[i] != 'X' && box[i] != 'O') {
-                boxAvailable = true;
-                break;
+    private static boolean noBoxAvailable(char[] box) {
+        for (int i = 0; i < 9; i++) {
+            if (box[i] == ' ') {
+                return false;
             }
         }
-        return boxAvailable;
+        return true;
     }
 
-    private static boolean checkDraw(boolean boxAvailable) {
-        if (boxAvailable == false) {
-            return true;
-        }
-        return false;
+    private static boolean checkDraw(char[] box, byte winner) {
+        return noBoxAvailable(box) && winner == 0;
     }
 
-    private interface Player {
+    public interface Player {
         public void makeTurn(char[] box);
     }
 
     private static class UserPlayer implements Player {
 
+        Scanner scan = new Scanner(System.in);
+
         @Override
         public void makeTurn(char[] box) {
+            if (noBoxAvailable(box)) {
+                return;
+            }
+
             byte input;
             while (true) {
                 input = scan.nextByte();
-                //check for inbounds
-                if (input > 0 && input < 10) {
-                    //check place availability
-                    if (box[input - 1] == 'X' || box[input - 1] == 'O')
-                        System.out.println("That one is already in use. Enter another.");
-                    else { // puts sign
-                        box[input - 1] = 'X';
-                        break;
-                    }
-                } else
+
+                if (input <= 0 || input >= 10) {
                     System.out.println("Invalid input. Enter again.");
+                    continue;
+                }
+
+                if (box[input - 1] != ' ') {
+                    System.out.println("That one is already in use. Enter another.");
+                    continue;
+                }
+
+                box[input - 1] = 'X';
+                break;
             }
         }
     }
@@ -155,10 +128,14 @@ public class App {
 
         @Override
         public void makeTurn(char[] box) {
+            if (noBoxAvailable(box)) {
+                return;
+            }
+
             byte rand;
             while (true) {
                 rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
-                if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
+                if (box[rand - 1] == ' ') {
                     box[rand - 1] = 'O';
                     break;
                 }
