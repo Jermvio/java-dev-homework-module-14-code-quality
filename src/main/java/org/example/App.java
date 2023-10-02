01eb330f583cb45000c2a6386319cc3df19e05b8
Jermvio <nikita.kabuzan@gmail.com>
2023-10-02 03:41:22 +0300

    Method extraction without logic changes. Blocks which were checking for bot and player win merged to one method.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,91 +1,153 @@
 package org.example;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
+        startGame();
+    }
+
+    private static void startGame() {
+        // init
+
         Scanner scan = new Scanner(System.in);
-        byte input;
-        byte rand;
-        byte i;
         boolean boxAvailable = false;
         byte winner = 0;
         char[] box = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
         System.out.println("Enter box number to select. Enjoy!\n");
 
         boolean boxEmpty = false;
+
+
         while (true) {
-            System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
-            if (!boxEmpty) {
-                for (i = 0; i < 9; i++)
-                    box[i] = ' ';
-                boxEmpty = true;
-            }
+            printTableInConsole(box);
 
-            if (winner == 1) {
-                System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
-                break;
-            } else if (winner == 2) {
-                System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
-                break;
-            } else if (winner == 3) {
-                System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+            boxEmpty = emptyBox(box, boxEmpty);
+
+            if (gameFinished(winner)) {
                 break;
             }
 
-            while (true) {
-                input = scan.nextByte();
-                if (input > 0 && input < 10) {
-                    if (box[input - 1] == 'X' || box[input - 1] == 'O')
-                        System.out.println("That one is already in use. Enter another.");
-                    else {
-                        box[input - 1] = 'X';
-                        break;
-                    }
-                } else
-                    System.out.println("Invalid input. Enter again.");
-            }
+            playerMakesMove(scan, box);
 
-            if ((box[0] == 'X' && box[1] == 'X' && box[2] == 'X') || (box[3] == 'X' && box[4] == 'X' && box[5] == 'X') || (box[6] == 'X' && box[7] == 'X' && box[8] == 'X') ||
-                    (box[0] == 'X' && box[3] == 'X' && box[6] == 'X') || (box[1] == 'X' && box[4] == 'X' && box[7] == 'X') || (box[2] == 'X' && box[5] == 'X' && box[8] == 'X') ||
-                    (box[0] == 'X' && box[4] == 'X' && box[8] == 'X') || (box[2] == 'X' && box[4] == 'X' && box[6] == 'X')) {
+            if (playerWin('X', box)) {
                 winner = 1;
                 continue;
             }
 
-            boxAvailable = false;
-            for (i = 0; i < 9; i++) {
-                if (box[i] != 'X' && box[i] != 'O') {
-                    boxAvailable = true;
-                    break;
-                }
-            }
+            boxAvailable = isBoxAvailable(box);
 
-            if (boxAvailable == false) {
+            if (checkDraw(boxAvailable)) {
                 winner = 3;
                 continue;
             }
 
-            while (true) {
-                rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
-                if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
-                    box[rand - 1] = 'O';
-                    break;
-                }
-            }
+            botMakesMove(box);
 
-            if ((box[0] == 'O' && box[1] == 'O' && box[2] == 'O') || (box[3] == 'O' && box[4] == 'O' && box[5] == 'O') || (box[6] == 'O' && box[7] == 'O' && box[8] == 'O') ||
-                    (box[0] == 'O' && box[3] == 'O' && box[6] == 'O') || (box[1] == 'O' && box[4] == 'O' && box[7] == 'O') || (box[2] == 'O' && box[5] == 'O' && box[8] == 'O') ||
-                    (box[0] == 'O' && box[4] == 'O' && box[8] == 'O') || (box[2] == 'O' && box[4] == 'O' && box[6] == 'O')) {
+            if (playerWin('O', box)) {
                 winner = 2;
                 continue;
             }
         }
-
     }
+
+    private static void printTableInConsole(char[] box) {
+        System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
+        System.out.println("-----------");
+        System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
+        System.out.println("-----------");
+        System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
+    }
+
+    private static boolean emptyBox(char[] box, boolean boxEmpty) {
+        byte i;
+        if (!boxEmpty) {
+            for (i = 0; i < 9; i++)
+                box[i] = ' ';
+            boxEmpty = true;
+        }
+        return boxEmpty;
+    }
+
+    private static boolean gameFinished(byte winner) {
+        boolean gameFinished = false;
+        if (winner == 1) {
+            System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
+            gameFinished = true;
+        } else if (winner == 2) {
+            System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
+            gameFinished = true;
+        } else if (winner == 3) {
+            System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+            gameFinished = true;
+        }
+        return gameFinished;
+    }
+
+    private static void playerMakesMove(Scanner scan, char[] box) {
+        byte input;
+        while (true) {
+            input = scan.nextByte();
+            //check for inbounds
+            if (input > 0 && input < 10) {
+                //check place availability
+                if (box[input - 1] == 'X' || box[input - 1] == 'O')
+                    System.out.println("That one is already in use. Enter another.");
+                else { // puts sign
+                    box[input - 1] = 'X';
+                    break;
+                }
+            } else
+                System.out.println("Invalid input. Enter again.");
+        }
+    }
+
+    private static boolean playerWin(char playerSign, char[] box) {
+        if (    (box[0] == playerSign && box[1] == playerSign && box[2] == playerSign) ||
+                (box[3] == playerSign && box[4] == playerSign && box[5] == playerSign) ||
+                (box[6] == playerSign && box[7] == playerSign && box[8] == playerSign) ||
+                (box[0] == playerSign && box[3] == playerSign && box[6] == playerSign) ||
+                (box[1] == playerSign && box[4] == playerSign && box[7] == playerSign) ||
+                (box[2] == playerSign && box[5] == playerSign && box[8] == playerSign) ||
+                (box[0] == playerSign && box[4] == playerSign && box[8] == playerSign) ||
+                (box[2] == playerSign && box[4] == playerSign && box[6] == playerSign)
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isBoxAvailable(char[] box) {
+        byte i;
+        boolean boxAvailable;
+        boxAvailable = false;
+        for (i = 0; i < 9; i++) {
+            if (box[i] != 'X' && box[i] != 'O') {
+                boxAvailable = true;
+                break;
+            }
+        }
+        return boxAvailable;
+    }
+
+    private static boolean checkDraw(boolean boxAvailable) {
+        if (boxAvailable == false) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void botMakesMove(char[] box) {
+        byte rand;
+        while (true) {
+            rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
+            if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
+                box[rand - 1] = 'O';
+                break;
+            }
+        }
+    }
+
 }
